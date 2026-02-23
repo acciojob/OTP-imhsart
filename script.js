d221548afa352405b868a69074ee1a044ecf803e
Nitin Rana <nitinrana2519@gmail.com>
2026-02-23 20:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll('.code')
+
+inputs.forEach((i)=> {
+	i.addEventListener('keydown', onKeyDown)
+	i.addEventListener('input', onInput)
+})
+
+function onKeyDown(e){
+	let curr = e.target
+	let prev = curr.previousElementSibling
+
+	if(e.key === 'Tab') return
+
+	if(e.key === 'Backspace'){
+		if(curr.value === '' && prev){
+			prev.focus()
+		}
+		return
+	}
+	if(e.key < '0' || e.key> '9'){
+		e.preventDefault()
+		return
+	}
+	if(curr.value.length === 1){
+		e.preventDefault()
+	}
+}
+
+function onInput(e){
+	let curr = e.target
+	let next = curr.nextElementSibling
+
+	if(curr.value.length === 1 && next){
+		next.focus()
+	}
+}
